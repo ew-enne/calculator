cca8314cb59099e04ec3b27f138d16cbb84e92b4
Ewenne <221577967+ew-enne@users.noreply.github.com>
2025-10-14 13:07:36 +0200

    Simplify transformEntry function
    Better solution : same result, much less code.

--- script.js
+++ script.js
@@ -35,51 +35,12 @@ function operate(a, b, operator) {
     }
 }
 
-// console.log(operate(3, 0, 'divide'));
 
-
-
-// transform clicked buttons to usable values
-function transformEntry(entry) {
-    switch (entry) {
-        case "btn-1":
-            return 1;
-        case "btn-2":
-            return 2;
-        case "btn-3":
-            return 3;
-        case "btn-4":
-            return 4;
-        case "btn-5":
-            return 5;
-        case "btn-6":
-            return 6;
-        case "btn-7":
-            return 7;
-        case "btn-8":
-            return 8;
-        case "btn-9":
-            return 9;
-        case "btn-0":
-            return 0;
-        case "btn-dot":
-            return '.';
-        case "btn-divide":
-            return 'divide';
-        case "btn-multiply":
-            return 'multiply';
-        case "btn-subtract":
-            return 'subtract';
-        case "btn-add":
-            return 'add';
-        case "btn-back":
-            return 'back';
-        case "btn-clear":
-            return 'clear';
-        case "btn-equal":
-            return 'equal';
-    };
+// transform clicked buttons to usable values (shorter version)
+function transformEntry(id) {
+    return id.replace('btn-', '');
 }
+
 
 // the readEntry function calls a callback each time input changes
 function readEntry(onNumberChange) {
