f711339e9d371104d7da7b9e6606bdd42781e726
Ewenne <221577967+ew-enne@users.noreply.github.com>
2025-10-21 11:54:02 +0200

    Add back function
    This allows to undo the last entry.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Global variables
-let globalNumber;
-let operator;
-let entryArray = [];
+
+let entryArray = ['0'];
 let operatorArray = [];
+let operator;
+let tempNumber;
 let firstNumber;
+let secondNumber;
 
 
 // Math functions --------
@@ -55,79 +57,94 @@ function transformEntry(id) {
 
 
 // Callback function for the readEntry function
-function handleNumberChange(entry) {
-    // Check if entry is a number
+function populateDisplay(entry) {
+    // Check if entry is not / is a number
     if (Number(entry) !== Number(entry)) {
         console.log("Entry is NOT a number");
+
     } else {
         console.log("Entry is a number");
-        globalNumber = document.querySelector(".screen").textContent = entry;
+        tempNumber = document.querySelector(".screen").textContent = entry;
     }
     
 }
 
 
-// The readEntry function sets up event listeners on the buttons and calls the callback function when input changes
+// Sets up event listeners on the buttons and calls the callback function when input changes
 function readEntry(onNumberChange) {
-    let digits = [];
+    let inputs = [];
     let buttons = document.querySelectorAll("button");
 
     buttons.forEach((button) => {
         button.addEventListener("click", event => {
             let buttonId = event.target.id;
 
-            //convert id to digit usable value using the transforEntry function
-            let digit = transformEntry(buttonId);
+            //convert id to usable value using the transforEntry function
+            let input = transformEntry(buttonId);
 
             // clear button resets everything
-            if (digit === 'clear') {
-                digits = [];
+            if (input === 'clear') {
+                inputs = [];
+                entryArray = [];
                 onNumberChange('0.00');
                 return;
             }
 
             // populate operatorArray when operators are pressed
-            if (digit === 'add') {
-                operatorArray.push(digit);
+            if (input === 'add') {
+                operatorArray.push(input);
+                console.log("Operator Array : ", operatorArray);
+                console.log("First number entered : ", tempNumber);
+                return;
+            }
+
+            if (input === 'subtract') {
+                operatorArray.push(input);
                 console.log("Operator Array : ", operatorArray);
                 return;
             }
 
-            if (digit === 'subtract') {
-                operatorArray.push(digit);
+            if (input === 'multiply') {
+                operatorArray.push(input);
                 console.log("Operator Array : ", operatorArray);
+                return;
             }
 
-            if (digit === 'multiply') {
-                operatorArray.push(digit);
+            if (input === 'divide') {
+                operatorArray.push(input);
                 console.log("Operator Array : ", operatorArray);
+                return;
             }
 
-            if (digit === 'divide') {
-                operatorArray.push(digit);
-                console.log("Operator Array : ", operatorArray);
-            }
-
-            // insert selected button value into digits array
+            // insert selected button value into inputs array
 
             // make sure that only one dot can be entered
-            if (digit === '.' && !entryArray.includes('.')) {
-                digits.push(digit);
-                entryArray = [...digits]; // creates a copy of digits array (not a reference to it)
-            } else if (digit === '.' && entryArray.includes('.')) {
+            if (input === '.' && !entryArray.includes('.')) {
+                inputs.push(input);
+                entryArray = [...inputs]; // creates a copy of inputs array (not a reference to it)
+            } else if (input === '.' && entryArray.includes('.')) {
                 return;
-            } else {
-            digits.push(digit);
-            entryArray = [...digits];
-}
+            } else if (input !== 'back') {
+                inputs.push(input);
+                //entryArray = [...inputs];
+            } else if (input === 'back') {     
+            // undo last entry (remove last element in the entryArray)            
+                inputs.splice(-1, 1);
+                console.log("Inputs Array one step back :", inputs);
+                // onNumberChange(number);
+            }
+
             // build number from array values and call callback
-            // let number = Number(digits.join(''));
-            let number = digits.join('');
+            // let number = Number(inputs.join(''));
+            let number = inputs.join('');
+            entryArray = [...inputs];
+            console.log("Entry Array is : ", entryArray);
             onNumberChange(number);
+            console.log("Temp. Number : ", tempNumber);
         });
     });
 }
 
 
 
-readEntry(handleNumberChange);
+readEntry(populateDisplay);
